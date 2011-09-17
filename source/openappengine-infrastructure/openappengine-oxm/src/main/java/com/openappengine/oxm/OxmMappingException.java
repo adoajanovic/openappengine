b5f28dfabac62efac017665d7e85353db0f67a55
hrishi2323 <hrishi2323@gmail.com>
2011-09-17 03:37:49 +0000

    Added openappengine-oxm to infrastructure layer.

---
+++ source/openappengine-infrastructure/openappengine-oxm/src/main/java/com/openappengine/oxm/OxmMappingException.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.openappengine.oxm;
+
+/**
+ * @author hrishi
+ *
+ */
+public class OxmMappingException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public OxmMappingException() {
+		super();
+	}
+
+	public OxmMappingException(String msg, Throwable t) {
+		super(msg, t);
+	}
+
+	public OxmMappingException(String msg) {
+		super(msg);
+	}
+
+	public OxmMappingException(Throwable t) {
+		super(t);
+	}
+
+}
