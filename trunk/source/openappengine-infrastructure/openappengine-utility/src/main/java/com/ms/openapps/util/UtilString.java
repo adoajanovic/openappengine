0e623624612f3e5fc24062e0f516f39eebcb9e40
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2011-09-18 14:14:22 +0000

    Added utility methods in UtilString and UtilXml

--- trunk/source/openappengine-infrastructure/openappengine-utility/src/main/java/com/ms/openapps/util/UtilString.java
+++ trunk/source/openappengine-infrastructure/openappengine-utility/src/main/java/com/ms/openapps/util/UtilString.java
@@ -20,5 +20,22 @@ public class UtilString {
 		}
 		return false;
 	}
+	
+	/**
+	 * @param s
+	 * @return boolean from String
+	 */
+	public static boolean convertToBoolean(String s) {
+		if(s == null || s.isEmpty()) {
+			return false;
+		}
+		boolean b = false;
+		try {
+		b = Boolean.parseBoolean(s);
+		} catch (Exception e) {
+			return false;
+		}
+		return b;
+	}
 
 }
