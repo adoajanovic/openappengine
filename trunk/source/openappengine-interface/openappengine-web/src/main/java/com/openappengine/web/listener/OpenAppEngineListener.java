4b992a00f580d9f168c64e2be82ca37fa33d33fe
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2011-09-20 02:43:31 +0000

    OpenAppEngineListener

--- trunk/source/openappengine-interface/openappengine-web/src/main/java/com/openappengine/web/listener/OpenAppEngineListener.java
+++ trunk/source/openappengine-interface/openappengine-web/src/main/java/com/openappengine/web/listener/OpenAppEngineListener.java
@@ -8,35 +8,36 @@ import com.openappengine.container.OpenAppEngineLifecycleHandler;
 
 /**
  * Application Lifecycle Listener implementation class OpenAppEngineListener
- *
+ * 
  */
 public final class OpenAppEngineListener implements ServletContextListener {
-	
+
 	private final UtilLogger logger = new UtilLogger(getClass());
+	
 	private final OpenAppEngineLifecycleHandler lifecycleHandler;
 
-    /**
-     * Default constructor. 
-     */
-    public OpenAppEngineListener() {
-    	lifecycleHandler = new OpenAppEngineLifecycleHandler();
-    	logger.logInfo("Initialized OpenAppEngineListener");
-    }
+	/**
+	 * Default constructor.
+	 */
+	public OpenAppEngineListener() {
+		lifecycleHandler = new OpenAppEngineLifecycleHandler();
+		logger.logInfo("Initialized OpenAppEngineListener");
+	}
 
 	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent servletContextEvent) {
-    	logger.logInfo("Booting up OpenAppEngine Components");
-    	lifecycleHandler.startup();
-    }
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		logger.logInfo("Booting up OpenAppEngine Components");
+		lifecycleHandler.startup();
+	}
 
 	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {
-    	logger.logInfo("Shutting Down OpenAppEngine Components");
-    	lifecycleHandler.shutdown();
-    }
-	
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		logger.logInfo("Shutting Down OpenAppEngine Components");
+		lifecycleHandler.shutdown();
+	}
+
 }
