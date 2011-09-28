5d781a1e25e54708dd40457a868e578bb3f06ccf
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2011-09-28 13:57:55 +0000

    Model API Changes.

---
+++ trunk/source/openappengine-infrastructure/openappengine-bpm/src/main/java/com/openappengine/bpm/model/Invoke.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.openappengine.bpm.model;
+
+import java.io.Serializable;
+
+/**
+ * @author hrishikesh.joshi
+ *
+ */
+public class Invoke implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ trunk/source/openappengine-infrastructure/openappengine-bpm/src/main/java/com/openappengine/bpm/service/model/DispatchContext.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.openappengine.bpm.service.model;
+
+/**
+ * @author hrishikesh.joshi
+ *
+ */
+public class DispatchContext {
+
+}
