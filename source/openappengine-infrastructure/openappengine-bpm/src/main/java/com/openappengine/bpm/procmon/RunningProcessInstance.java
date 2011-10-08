b3defb00fb75f7b1d3aff94a1f587e3d86967453
hrishi2323 <hrishi2323@gmail.com>
2011-10-08 03:16:12 +0000

    Changes in ProcMon. Added a draft ProcessRegistry package.

--- source/openappengine-infrastructure/openappengine-bpm/src/main/java/com/openappengine/bpm/procmon/RunningProcessInstance.java
+++ source/openappengine-infrastructure/openappengine-bpm/src/main/java/com/openappengine/bpm/procmon/RunningProcessInstance.java
@@ -22,11 +22,17 @@ public class RunningProcessInstance implements Serializable {
 	 *  <code>ProcessStatus</code> of this process running in the ProcessEngine.
 	 */
 	private ProcessStatus processStatus;
+	
+	/**
+	 *  The lookup key to the process defined in the <code>ProcessRegistry</code>.
+	 */
+	private String processRegistryId;
 
 	/**
 	 * Default Constructor
 	 */
-	public RunningProcessInstance() {
+	public RunningProcessInstance(String procRegistryId) {
+		this.processRegistryId = procRegistryId;
 		pid = PIDGenerator.newProcessId();
 		processStatus = new ProcessStatus();
 	}
@@ -42,6 +48,20 @@ public class RunningProcessInstance implements Serializable {
 
 	public void setProcessStatus(ProcessStatus processStatus) {
 		this.processStatus = processStatus;
+	}
+
+	/**
+	 * @return the processRegistryId
+	 */
+	public String getProcessRegistryId() {
+		return processRegistryId;
+	}
+
+	/**
+	 * @param processRegistryId the processRegistryId to set
+	 */
+	public void setProcessRegistryId(String processRegistryId) {
+		this.processRegistryId = processRegistryId;
 	}
 
 }
