a5e4bed5245d33a6c224e0b041524bd7ce580ad9
hrishi2323 <hrishi2323@gmail.com>
2011-10-15 16:55:23 +0000

    Changes in the ProcessDefReader API.

--- source/openappengine-infrastructure/openappengine-bpm/src/test/java/com/openappengine/bpm/procreader/TestProcessDefReader.java
+++ source/openappengine-infrastructure/openappengine-bpm/src/test/java/com/openappengine/bpm/procreader/TestProcessDefReader.java
@@ -8,9 +8,6 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import com.openappengine.bpm.graph.ProcessDefinition;
-import com.openappengine.bpm.procreader.IProcessDefReader;
-import com.openappengine.bpm.procreader.ProcessDefReader;
-import com.openappengine.bpm.procreader.ProcessDefinitionException;
 
 
 /**
@@ -21,7 +18,7 @@ public class TestProcessDefReader {
 	
 	@Test
 	public void testReadProcessDefinition() throws ProcessDefinitionException {
-		IProcessDefReader processDefReader = new ProcessDefReader(getClass().getClassLoader().getResourceAsStream("TestProcessDef.xml"));
+		ProcessDefReader processDefReader = new ProcessDefReader(getClass().getClassLoader().getResourceAsStream("TestProcessDef.xml"));
 		ProcessDefinition processDefinition = processDefReader.readProcessDefinition();
 		Assert.assertNotNull("ProcessDefinition found null..", processDefinition);
 	}
