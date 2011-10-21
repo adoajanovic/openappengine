dd33eb7b0a3c56196f91a9d3ca0bdd623be7f5ff
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2011-10-21 03:15:26 +0000

    Changes in the model - Party, Inventory, SalesOrder, Item

--- trunk/source/openappengine-domain/openappengine-model/openappengine-model/src/main/java/com/openappengine/model/party/PartyContactMech.java
+++ trunk/source/openappengine-domain/openappengine-model/openappengine-model/src/main/java/com/openappengine/model/party/PartyContactMech.java
@@ -1,12 +1,10 @@
 package com.openappengine.model.party;
 
-import java.io.Serializable;
+import com.openappengine.model.valueobject.ValueObject;
 
 
-public class PartyContactMech implements Serializable {
+public class PartyContactMech implements ValueObject<PartyContactMech> {
 	
-	private static final long serialVersionUID = 1L;
-
 	private int contactMechId;
 
 	private String contactMechPurpose;
@@ -59,5 +57,51 @@ public class PartyContactMech implements Serializable {
 	public void setParty(Party party) {
 		this.party = party;
 	}
-    
+
+	@Override
+	public boolean sameValueAs(PartyContactMech other) {
+		return this.equals(other);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((contactMechPurpose == null) ? 0 : contactMechPurpose
+						.hashCode());
+		result = prime * result
+				+ ((contactMechType == null) ? 0 : contactMechType.hashCode());
+		result = prime * result
+				+ ((infoString == null) ? 0 : infoString.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PartyContactMech other = (PartyContactMech) obj;
+		if (contactMechPurpose == null) {
+			if (other.contactMechPurpose != null)
+				return false;
+		} else if (!contactMechPurpose.equals(other.contactMechPurpose))
+			return false;
+		if (contactMechType == null) {
+			if (other.contactMechType != null)
+				return false;
+		} else if (!contactMechType.equals(other.contactMechType))
+			return false;
+		if (infoString == null) {
+			if (other.infoString != null)
+				return false;
+		} else if (!infoString.equals(other.infoString))
+			return false;
+		return true;
+	}
 }
