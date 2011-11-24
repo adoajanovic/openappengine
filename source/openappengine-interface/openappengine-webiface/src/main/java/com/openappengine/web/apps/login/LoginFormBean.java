37a952fb222c263381ca07122293c34bcd7166d9
hrishi2323 <hrishi2323@gmail.com>
2011-11-24 19:17:20 +0000

    Corrected the jstl tag.

---
+++ source/openappengine-interface/openappengine-webiface/src/main/java/com/openappengine/web/apps/login/LoginFormBean.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.openappengine.web.apps.login;
+
+import java.io.Serializable;
+
+/**
+ * @author hrishi
+ *
+ */
+public class LoginFormBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String login() {
+		return null;
+	}
+
+}
