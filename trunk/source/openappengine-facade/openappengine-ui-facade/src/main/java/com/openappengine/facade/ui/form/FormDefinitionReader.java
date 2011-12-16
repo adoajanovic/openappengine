f08f0e06581e65f8ed85d7ef04902a8814aee7c3
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2011-12-16 14:31:37 +0000

    Changes in the FieldDefinitionReader (FieldLayout and IncludeAll)

--- trunk/source/openappengine-facade/openappengine-ui-facade/src/main/java/com/openappengine/facade/ui/form/FormDefinitionReader.java
+++ trunk/source/openappengine-facade/openappengine-ui-facade/src/main/java/com/openappengine/facade/ui/form/FormDefinitionReader.java
@@ -6,11 +6,14 @@ package com.openappengine.facade.ui.form;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.lang.Validate;
 import org.apache.log4j.Logger;
 
 import com.openappengine.facade.ui.common.EntityReference;
+import com.openappengine.facade.ui.common.EntityReference.IncludeFields;
+import com.openappengine.facade.ui.common.FieldReference;
 import com.openappengine.facade.ui.common.RenderMode;
 import com.thoughtworks.xstream.XStream;
 
@@ -24,6 +27,8 @@ public class FormDefinitionReader {
 
 	protected final Logger logger = Logger.getLogger(getClass());
 
+	private boolean validate;
+
 	public List<FormDefinition> readFormDefinitions() {
 		Validate.notEmpty(getLocations(), "No Location specified for FormDefinitions.");
 		List<FormDefinition> allFormDefinitions = new ArrayList<FormDefinition>();
@@ -34,8 +39,10 @@ public class FormDefinitionReader {
 		xStream.aliasType("render-mode", RenderMode.class);
 		xStream.aliasType("form-type", FormType.class);
 		xStream.aliasType("form-layout", FieldLayout.class);
+		xStream.aliasType("field-reference", FieldReference.class);
 		
 		xStream.addImplicitCollection(FormDefinitionContext.class, "formDefinitions");
+		xStream.addImplicitCollection(FieldLayout.class, "fieldReferences");
 		
 		xStream.aliasField("entity-reference", FormDefinition.class, "entityReference");
 		xStream.aliasField("field-layout", FormDefinition.class, "fieldLayout");
@@ -45,10 +52,12 @@ public class FormDefinitionReader {
 		xStream.useAttributeFor(FormDefinition.class, "name");
 		xStream.useAttributeFor(EntityReference.class, "entityName");
 		xStream.useAttributeFor(EntityReference.class, "includeFields");
+		xStream.useAttributeFor(FieldReference.class, "fieldName");
 		
         xStream.aliasField("name", FormDefinition.class, "name");
         xStream.aliasField("entityName", EntityReference.class, "entityName");
         xStream.aliasField("includeFields", EntityReference.class, "include-fields");
+        xStream.aliasField("fieldName", FieldReference.class, "field-name");
         
 		for (String location : getLocations()) {
 			FormDefinitionContext formDefinitionContext = readFormDefinition(xStream, location);
@@ -69,37 +78,32 @@ public class FormDefinitionReader {
 		try {
 			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(location);
 			FormDefinitionContext formDefinitionContext = (FormDefinitionContext) xStream.fromXML(inputStream);
+			if(isValidate()) {
+				if(formDefinitionContext != null) {
+					Set<FormDefinition> formDefinitions = formDefinitionContext.getFormDefinitions();
+					if(formDefinitions != null) {
+						for (FormDefinition formDefinition : formDefinitions) {
+							// TODO - Handle Validations As per the business
+							// logic. Throw RuntimeExceptions if some
+							// business/application logic fails...Create a seperate Validator API for this.
+							EntityReference entityReference = formDefinition.getEntityReference();
+							if(entityReference != null) {
+								IncludeFields includeFields = entityReference.getIncludeFields();
+								if(includeFields.equals(IncludeFields.REFERENCED)) {
+									if (formDefinition.getFieldLayout() == null || formDefinition.getFieldLayout().getFieldReferences().isEmpty()) {
+										throw new FormDefinitionException(
+												"If IncludeFields=REFERENCED, Fields should be referred in the <field-layout> tag");
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 			return formDefinitionContext;
 		} catch (Throwable e) {
 			throw new FormDefinitionException("Exception encountered while reading the FormDefinition", e);
 		}
-	}
-
-	/**
-	 * 
-	 */
-	protected void test() {
-		FormDefinition formDefinition = new FormDefinition();
-		formDefinition.setEntityReference(new EntityReference("CodeType"));
-		formDefinition.setFieldLayout(FieldLayout.getDefault());
-		formDefinition.setFormType(FormType.SIMPLE);
-		formDefinition.setRenderMode(RenderMode.READ_WRITE);
-
-		FormDefinitionContext formDefinitionContext = new FormDefinitionContext();
-		formDefinitionContext.addFormDefinition(formDefinition);
-
-		XStream xStream = new XStream();
-		// form-definitions : Root Tag
-		xStream.aliasType("form-definitions", FormDefinitionContext.class);
-		xStream.addImplicitCollection(FormDefinitionContext.class, "formDefinitions");
-		xStream.aliasType("form-definition", FormDefinition.class);
-		xStream.aliasType("entity-reference", EntityReference.class);
-		xStream.aliasType("form-layout", FieldLayout.class);
-		xStream.aliasType("render-mode", RenderMode.class);
-		xStream.aliasType("form-type", FormType.class);
-
-		String xml = xStream.toXML(formDefinitionContext);
-		System.out.println(xml);
 	}
 
 	protected String[] getLocations() {
@@ -108,6 +112,14 @@ public class FormDefinitionReader {
 
 	public void setLocations(String[] locations) {
 		this.locations = locations;
+	}
+
+	public boolean isValidate() {
+		return validate;
+	}
+
+	public void setValidate(boolean validate) {
+		this.validate = validate;
 	}
 
 }
