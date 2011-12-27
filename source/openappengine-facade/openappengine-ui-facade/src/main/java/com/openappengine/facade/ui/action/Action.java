9e8dcd060319cd5ed73fb019eb1155ef4d3e2ea8
hrishi2323 <hrishi2323@gmail.com>
2011-12-27 02:38:57 +0000

    Added conditional Actions to the Action API.

---
+++ source/openappengine-facade/openappengine-ui-facade/src/main/java/com/openappengine/facade/ui/action/Action.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.openappengine.facade.ui.action;
+
+/**
+ * @author hrishi
+ *
+ */
+public interface Action extends Executable {
+
+}
---
+++ source/openappengine-facade/openappengine-ui-facade/src/main/java/com/openappengine/facade/ui/action/ActionWrapper.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.openappengine.facade.ui.action;
+
+import org.apache.log4j.Logger;
+
+import com.openappengine.facade.ui.context.ScreenContext;
+import com.openappengine.facade.ui.expression.ConditionExpressionEvaluator;
+
+
+/**
+ * @author hrishi
+ *
+ */
+public abstract class ActionWrapper implements Action {
+	
+	protected final Logger logger = Logger.getLogger(getClass());
+
+	protected Action wrappedAction;
+	
+	private ConditionExpressionEvaluator evaluator;
+	
+	protected Action getWrappedAction() {
+		return wrappedAction;
+	}
+
+	protected abstract void setWrappedAction(Action action);
+
+	@Override
+	public Object execute(ScreenContext screenContext) {
+		return wrappedAction.execute(screenContext);
+	}
+	
+	protected Boolean evaluateConditionExpression(ScreenContext context,String expression) {
+		if(evaluator == null) {
+			evaluator = new ConditionExpressionEvaluator();
+		}
+		Boolean conditionEvaluation = evaluator.evaluate(context,expression);
+		return conditionEvaluation;
+	}
+}
