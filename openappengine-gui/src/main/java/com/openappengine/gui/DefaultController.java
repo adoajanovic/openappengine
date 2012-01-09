ad887a7900b65355854b8ccda31db20e16da7bdd
hrishi2323 <hrishi2323@gmail.com>
2012-01-09 12:21:08 +0000

    Sitemesh Freemarker Integration Added CSS

--- openappengine-gui/src/main/java/com/openappengine/gui/DefaultController.java
+++ openappengine-gui/src/main/java/com/openappengine/gui/DefaultController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,25 +16,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class DefaultController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	@RequestMapping(value = "/site/{name}", method = RequestMethod.GET)
+	public String handleRequest(@PathVariable("name") String  name,Locale locale, Model model) {
 		logger.info("Welcome home! the client locale is "+ locale.toString());
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
-		
 		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return name;
 	}
 	
 }
---
+++ openappengine-gui/src/main/webapp/WEB-INF/freemarker/home.ftl
@@ -0,0 +1,24 @@
+<html>
+<#import "/spring.ftl" as spring/>
+<head>
+  <title>
+  	Test
+  </title>
+</head>
+
+<body id="index">					
+        
+ <#if serverTime?exists>
+	<div style="z-index: 99; position: absolute; left: 200px;">
+	 <div class="message">${serverTime}</div>
+	</div>
+ </#if>			  	 	  
+ 
+ <br/><br/>
+ 	
+ <div id="main">
+ 	Main Content 	  	
+ </div>
+    
+</body>
+</html>
