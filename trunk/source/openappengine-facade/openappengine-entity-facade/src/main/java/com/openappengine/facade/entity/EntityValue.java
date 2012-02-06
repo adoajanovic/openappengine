c8f0c88eb003dc993824b7908a3a9c6126347033
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2012-02-06 14:30:56 +0000

    Extracted an EntityValue interface.

--- trunk/source/openappengine-facade/openappengine-entity-facade/src/main/java/com/openappengine/facade/entity/EntityValue.java
+++ trunk/source/openappengine-facade/openappengine-entity-facade/src/main/java/com/openappengine/facade/entity/EntityValue.java
@@ -3,60 +3,21 @@
  */
 package com.openappengine.facade.entity;
 
-import java.io.Serializable;
-
 import com.openappengine.facade.entity.definition.EntityDefinition;
 
 /**
  * @author hrishikesh.joshi
+ * @since  Feb 6, 2012
  *
  */
-public class EntityValue extends DataBeanWrapper implements Serializable {
+public interface EntityValue {
+	
+	String getEntityName();
+	
+	Object getInstance();
+	
+	void setInstance(Object value);
+	
+	EntityDefinition getEntityDefinition();
 
-	private static final long serialVersionUID = 1L;
-	
-	private Object instance;
-	
-	private String entityName;
-	
-	private Serializable entityId;
-	
-	private EntityDefinition entityDefinition;
-	
-	public EntityValue(String entityName,EntityDefinition entityDefinition,Object object) {
-	    super(object);
-	    this.entityName = entityName;
-	    this.entityDefinition = entityDefinition;
-	    this.setInstance(object);
-	}
-	
-	public boolean isModified() {
-		//TODO
-		return false;
-	}
-	
-	public Object getInstance() {
-	    return instance;
-	}
-
-	public void setInstance(Object instance) {
-		this.instance = instance;
-	}
-
-	public String getEntityName() {
-	    return entityName;
-	}
-
-	public EntityDefinition getEntityDefinition() {
-	    return entityDefinition;
-	}
-
-	public Serializable getEntityId() {
-		return entityId;
-	}
-
-	public void setEntityId(Serializable entityId) {
-		this.entityId = entityId;
-	}
-	
 }
