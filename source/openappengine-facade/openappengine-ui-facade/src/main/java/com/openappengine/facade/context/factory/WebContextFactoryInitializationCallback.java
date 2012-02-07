1b9ab523e3dc0f371acba15a7727c41f1235974e
hrishi2323 <hrishi2323@gmail.com>
2012-02-07 14:40:46 +0000

    Changed the names for GuiContextRestoreEvent and GuiContextRestoreEventProcessor to
    ExecutePreRenderActionsEvent and 
    ExecutePreRenderActionsEventProcessor respectively.

--- source/openappengine-facade/openappengine-ui-facade/src/main/java/com/openappengine/facade/context/factory/WebContextFactoryInitializationCallback.java
+++ source/openappengine-facade/openappengine-ui-facade/src/main/java/com/openappengine/facade/context/factory/WebContextFactoryInitializationCallback.java
@@ -6,11 +6,11 @@ package com.openappengine.facade.context.factory;
 import com.openappengine.facade.core.context.LifecycleProcessor;
 import com.openappengine.facade.core.context.event.ContextInitializedEvent;
 import com.openappengine.facade.core.context.event.ContextPostRestoreEvent;
-import com.openappengine.facade.core.context.event.ContextRestoreEvent;
+import com.openappengine.facade.core.context.event.ExecutePreRenderActionsEvent;
 import com.openappengine.facade.core.context.event.GuiContextMessageRefreshEvent;
 import com.openappengine.facade.core.context.event.processor.GuiContextInitializedEventProcessor;
 import com.openappengine.facade.core.context.event.processor.GuiContextPostRestoreEventProcessor;
-import com.openappengine.facade.core.context.event.processor.GuiContextRestoreEventProcessor;
+import com.openappengine.facade.core.context.event.processor.ExecutePreRenderActionsEventProcessor;
 import com.openappengine.facade.core.context.event.processor.UpdateMessageEventProcessor;
 import com.openappengine.facade.core.context.lifecycle.DefaultLifecycleProcessor;
 
@@ -53,7 +53,7 @@ public class WebContextFactoryInitializationCallback extends ContextFactoryIniti
 	 */
 	protected void registerApplicationLifecycleEventProcessors(DefaultLifecycleProcessor lifecycleProcessor) {
 		lifecycleProcessor.registerLifecycleEventProcessor(ContextPostRestoreEvent.class,new GuiContextPostRestoreEventProcessor());
-		lifecycleProcessor.registerLifecycleEventProcessor(ContextRestoreEvent.class,new GuiContextRestoreEventProcessor());
+		lifecycleProcessor.registerLifecycleEventProcessor(ExecutePreRenderActionsEvent.class,new ExecutePreRenderActionsEventProcessor());
 		lifecycleProcessor.registerLifecycleEventProcessor(ContextInitializedEvent.class,new GuiContextInitializedEventProcessor());
 		lifecycleProcessor.registerLifecycleEventProcessor(GuiContextMessageRefreshEvent.class,new UpdateMessageEventProcessor());
 		//TODO - Add Other Event Processors.
