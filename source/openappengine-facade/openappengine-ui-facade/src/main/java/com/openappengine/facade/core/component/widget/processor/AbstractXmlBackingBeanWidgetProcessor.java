590370554f432980055121dae4b6262f64f4b1e9
hrishi2323 <hrishi2323@gmail.com>
2012-02-09 14:33:00 +0000

    Misc Changes for WidgetProcessorContext

--- source/openappengine-facade/openappengine-ui-facade/src/main/java/com/openappengine/facade/core/component/widget/processor/AbstractXmlBackingBeanWidgetProcessor.java
+++ source/openappengine-facade/openappengine-ui-facade/src/main/java/com/openappengine/facade/core/component/widget/processor/AbstractXmlBackingBeanWidgetProcessor.java
@@ -4,21 +4,26 @@
 package com.openappengine.facade.core.component.widget.processor;
 
 import com.openappengine.facade.core.component.widget.context.WidgetProcessorContext;
+import com.openappengine.facade.entity.EntityValue;
 
 /**
  * @author hrishikesh.joshi
  * @since  Feb 7, 2012
  *
  */
-public abstract class XmlBackingBeanWidgetProcessor implements WidgetProcessor {
+public abstract class AbstractXmlBackingBeanWidgetProcessor implements WidgetProcessor {
 	
 	private WidgetProcessorContext context;
 
 	@Override
 	public void setWidgetProcessorContext(WidgetProcessorContext context) {
+		if(context == null) {
+			throw new IllegalArgumentException("WidgetProcessorContext cannot be null.");
+		}
 		this.context = context;
 	}
 
+	//TODO - To be removed.
 	@Override
 	public Class<?> getWidgetProcessorContextClass() {
 		return context.getClass();
@@ -26,7 +31,11 @@ public abstract class XmlBackingBeanWidgetProcessor implements WidgetProcessor {
 
 	@Override
 	public Object processWidget() {
-		// TODO Auto-generated method stub
+		EntityValue ev = (EntityValue) context.getELContext().getVariable(context.getWidgetBackingObjectValueRef());
+		if(ev != null) {
+			Object previousInstance = ev.getInstance();
+			
+		}
 		return null;
 	}
 	
