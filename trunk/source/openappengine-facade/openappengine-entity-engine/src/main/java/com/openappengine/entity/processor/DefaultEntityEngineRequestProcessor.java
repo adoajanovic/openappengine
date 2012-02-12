bb426c36536f3c79a71580fa528cf98debb13f64
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2012-02-12 14:17:15 +0000

    EntityEngine package name changes.

---
+++ trunk/source/openappengine-facade/openappengine-entity-engine/src/main/java/com/openappengine/entity/processor/DefaultEntityEngineRequestProcessor.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.openappengine.entity.processor;
+
+import com.openappengine.entity.request.EntityRequest;
+import com.openappengine.entity.response.EntityResponse;
+
+/**
+ * @author hrishi
+ * since Feb 12, 2012
+ */
+public class DefaultEntityEngineRequestProcessor implements EntityEngineRequestProcessor {
+
+	@Override
+	public EntityResponse process(EntityRequest request) {
+		return null;
+	}
+
+}
