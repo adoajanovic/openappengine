523f6eb46b0e602d27a08fe1738ef5efd54db89d
hrishi2323 <hrishi2323@gmail.com>
2012-03-06 02:49:35 +0000

    Added Package api and delegator.

---
+++ source/openappengine-facade/openappengine-entity-engine/src/main/java/com/openappengine/entity/api/ValueEntity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.openappengine.entity.api;
+
+import com.openappengine.entity.model.ModelEntity;
+
+/**
+ * @author hrishi
+ * since Mar 5, 2012
+ */
+public class ValueEntity extends GenericEntity {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected ValueEntity(){}
+
+	//Create a new Value Entity.
+	public static ValueEntity createValueEntity(ModelEntity modelEntity) {
+		return null;
+	}
+	
+}
---
+++ source/openappengine-facade/openappengine-entity-engine/src/main/java/com/openappengine/entity/delegator/Delegator.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.openappengine.entity.delegator;
+
+/**
+ * @author hrishi
+ * since Mar 5, 2012
+ */
+public interface Delegator {
+
+}
