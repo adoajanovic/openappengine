d4ceab65e647fe191403bcc6aec14762f0c5acf1
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2012-03-17 11:18:05 +0000

    EntityEngineChanges. FTL Changes. ServiceEngine POM and Config changes.

---
+++ trunk/source/openappengine-facade/openappengine-entity-engine/src/main/java/com/openappengine/entity/datasource/BaseEntityDao.java
@@ -0,0 +1,18 @@
+package com.openappengine.entity.datasource;
+
+import com.openappengine.entity.api.ValueEntity;
+
+/**
+ * 
+ * @author hrishi
+ * since Mar 17, 2012
+ */
+public class BaseEntityDao implements EntityDao {
+
+	@Override
+	public int save(ValueEntity valueEntity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ trunk/source/openappengine-facade/openappengine-entity-engine/src/main/java/com/openappengine/entity/datasource/EntityException.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.openappengine.entity.datasource;
+
+/**
+ * @author hrishi
+ * since Mar 17, 2012
+ */
+public class EntityException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
