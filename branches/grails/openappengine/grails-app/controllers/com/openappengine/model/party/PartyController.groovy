dbf1ddcb17aa775c6401855869396b51afefe6fe
hrishi2323@gmail.com <hrishi2323@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2012-05-16 02:47:25 +0000

    Added jquery-ui.

--- branches/grails/openappengine/grails-app/controllers/com/openappengine/model/party/PartyController.groovy
+++ branches/grails/openappengine/grails-app/controllers/com/openappengine/model/party/PartyController.groovy
@@ -1,103 +1,155 @@
 package com.openappengine.model.party
 
+import grails.converters.JSON
 import org.springframework.dao.DataIntegrityViolationException
 
 class PartyController {
 
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-    def index() {
-        redirect(action: "list", params: params)
-    }
+	def index() {
+		redirect(action: "list", params: params)
+	}
 
-    def list() {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [partyInstanceList: Party.list(params), partyInstanceTotal: Party.count()]
-    }
+	def list() {
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		[partyInstanceList: Party.list(params), partyInstanceTotal: Party.count()]
+	}
 
-    def create() {
-        [partyInstance: new Party(params)]
-    }
+	def create() {
+		[partyInstance: new Party(params)]
+	}
 
-    def save() {
-        def partyInstance = new Party(params)
-        if (!partyInstance.save(flush: true)) {
-            render(view: "create", model: [partyInstance: partyInstance])
-            return
-        }
+	def save() {
+		def partyInstance = new Party(params)
+		if (!partyInstance.save(flush: true)) {
+			render(view: "create", model: [partyInstance: partyInstance])
+			return
+		}
 
-		flash.message = message(code: 'default.created.message', args: [message(code: 'party.label', default: 'Party'), partyInstance.id])
-        redirect(action: "show", id: partyInstance.id)
-    }
+		flash.message = message(code: 'default.created.message', args: [
+			message(code: 'party.label', default: 'Party'),
+			partyInstance.id
+		])
+		redirect(action: "show", id: partyInstance.id)
+	}
 
-    def show() {
-        def partyInstance = Party.get(params.id)
-        if (!partyInstance) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: 'party.label', default: 'Party'), params.id])
-            redirect(action: "list")
-            return
-        }
+	def show() {
+		def partyInstance = Party.get(params.id)
+		if (!partyInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'party.label', default: 'Party'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
 
-        [partyInstance: partyInstance]
-    }
+		[partyInstance: partyInstance]
+	}
 
-    def edit() {
-        def partyInstance = Party.get(params.id)
-        if (!partyInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'party.label', default: 'Party'), params.id])
-            redirect(action: "list")
-            return
-        }
+	def edit() {
+		def partyInstance = Party.get(params.id)
+		if (!partyInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'party.label', default: 'Party'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
 
-        [partyInstance: partyInstance]
-    }
+		[partyInstance: partyInstance]
+	}
 
-    def update() {
-        def partyInstance = Party.get(params.id)
-        if (!partyInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'party.label', default: 'Party'), params.id])
-            redirect(action: "list")
-            return
-        }
+	def update() {
+		def partyInstance = Party.get(params.id)
+		if (!partyInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'party.label', default: 'Party'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
 
-        if (params.version) {
-            def version = params.version.toLong()
-            if (partyInstance.version > version) {
-                partyInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'party.label', default: 'Party')] as Object[],
-                          "Another user has updated this Party while you were editing")
-                render(view: "edit", model: [partyInstance: partyInstance])
-                return
-            }
-        }
+		if (params.version) {
+			def version = params.version.toLong()
+			if (partyInstance.version > version) {
+				partyInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+						[
+							message(code: 'party.label', default: 'Party')]
+						as Object[],
+						"Another user has updated this Party while you were editing")
+				render(view: "edit", model: [partyInstance: partyInstance])
+				return
+			}
+		}
 
-        partyInstance.properties = params
+		partyInstance.properties = params
 
-        if (!partyInstance.save(flush: true)) {
-            render(view: "edit", model: [partyInstance: partyInstance])
-            return
-        }
+		if (!partyInstance.save(flush: true)) {
+			render(view: "edit", model: [partyInstance: partyInstance])
+			return
+		}
 
-		flash.message = message(code: 'default.updated.message', args: [message(code: 'party.label', default: 'Party'), partyInstance.id])
-        redirect(action: "show", id: partyInstance.id)
-    }
+		flash.message = message(code: 'default.updated.message', args: [
+			message(code: 'party.label', default: 'Party'),
+			partyInstance.id
+		])
+		redirect(action: "show", id: partyInstance.id)
+	}
 
-    def delete() {
-        def partyInstance = Party.get(params.id)
-        if (!partyInstance) {
-			flash.message = message(code: 'default.not.found.message', args: [message(code: 'party.label', default: 'Party'), params.id])
-            redirect(action: "list")
-            return
-        }
+	def delete() {
+		def partyInstance = Party.get(params.id)
+		if (!partyInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'party.label', default: 'Party'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
 
-        try {
-            partyInstance.delete(flush: true)
-			flash.message = message(code: 'default.deleted.message', args: [message(code: 'party.label', default: 'Party'), params.id])
-            redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
-			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'party.label', default: 'Party'), params.id])
-            redirect(action: "show", id: params.id)
-        }
-    }
+		try {
+			partyInstance.delete(flush: true)
+			flash.message = message(code: 'default.deleted.message', args: [
+				message(code: 'party.label', default: 'Party'),
+				params.id
+			])
+			redirect(action: "list")
+		}
+		catch (DataIntegrityViolationException e) {
+			flash.message = message(code: 'default.not.deleted.message', args: [
+				message(code: 'party.label', default: 'Party'),
+				params.id
+			])
+			redirect(action: "show", id: params.id)
+		}
+	}
+
+	def listJSON = {
+		def sortIndex = params.sidx ?: 'name'
+		def sortOrder  = params.sord ?: 'asc'
+		def maxRows = Integer.valueOf(params.rows)
+		def currentPage = Integer.valueOf(params.page) ?: 1
+		def rowOffset = currentPage == 1 ? 0 : (currentPage - 1) * maxRows
+		def contacts = Party.list (params)
+		def totalRows = Party.count()
+		def numberOfPages = Math.ceil(totalRows / maxRows)
+
+		def results = contacts?.collect {
+			[
+						cell: [
+							it.externalId,
+							it.description,
+							it.status
+						],
+						id: it.partyId
+					]
+		}
+
+		def jsonData = [rows: results, page: currentPage, records: totalRows, total: numberOfPages]
+		render jsonData as JSON
+	}
 }
--- branches/grails/openappengine/grails-app/views/party/list.gsp
+++ branches/grails/openappengine/grails-app/views/party/list.gsp
@@ -6,6 +6,32 @@
 		<meta name="layout" content="main">
 		<g:set var="entityName" value="${message(code: 'party.label', default: 'Party')}" />
 		<title><g:message code="default.list.label" args="[entityName]" /></title>
+		<jq:resources />
+        <jqui:resources theme="smoothness" />
+        <jqgrid:resources />
+        <script type="text/javascript">
+	        $(document).ready(function() {
+                         <jqgrid:grid
+                                id="party"
+                                url="'${createLink(action: 'listJSON')}'"
+                                editurl="'${createLink(action: 'editJSON')}'"
+                                colNames="'External Id', 'Status', 'Description'"
+                                colModel="{name:'externalId', editable: false},
+                                          {name:'status', editable: true},
+                                          {name:'description', editable: true}"
+                                sortname="'externalId'"
+                                caption="'Party List'"
+                                height="300"
+                                autowidth="true"
+                                scrollOffset="0"
+                                viewrecords="true"
+                                showPager="true"
+                                datatype="'json'">
+                                <jqgrid:navigation id="partyStandard" add="true" edit="true" 
+                                      del="true" search="true" refresh="true" />
+                         </jqgrid:grid>
+               });
+        </script>
 	</head>
 	<body>
 		<a href="#list-party" class="skip" tabindex="-1"><g:message code="default.link.skip.label" default="Skip to content&hellip;"/></a>
@@ -20,47 +46,9 @@
 			<g:if test="${flash.message}">
 			<div class="message" role="status">${flash.message}</div>
 			</g:if>
-			<table>
-				<thead>
-					<tr>
-					
-						<g:sortableColumn property="partyId" title="${message(code: 'party.partyId.label', default: 'Party Id')}" />
-					
-						<g:sortableColumn property="description" title="${message(code: 'party.description.label', default: 'Description')}" />
-					
-						<g:sortableColumn property="externalId" title="${message(code: 'party.externalId.label', default: 'External Id')}" />
-					
-						<g:sortableColumn property="partyType" title="${message(code: 'party.partyType.label', default: 'Party Type')}" />
-					
-						<g:sortableColumn property="preferredCurrencyUom" title="${message(code: 'party.preferredCurrencyUom.label', default: 'Preferred Currency Uom')}" />
-					
-						<g:sortableColumn property="status" title="${message(code: 'party.status.label', default: 'Status')}" />
-					
-					</tr>
-				</thead>
-				<tbody>
-				<g:each in="${partyInstanceList}" status="i" var="partyInstance">
-					<tr class="${(i % 2) == 0 ? 'even' : 'odd'}">
-					
-						<td><g:link action="show" id="${partyInstance.id}">${fieldValue(bean: partyInstance, field: "partyId")}</g:link></td>
-					
-						<td>${fieldValue(bean: partyInstance, field: "description")}</td>
-					
-						<td>${fieldValue(bean: partyInstance, field: "externalId")}</td>
-					
-						<td>${fieldValue(bean: partyInstance, field: "partyType")}</td>
-					
-						<td>${fieldValue(bean: partyInstance, field: "preferredCurrencyUom")}</td>
-					
-						<td>${fieldValue(bean: partyInstance, field: "status")}</td>
-					
-					</tr>
-				</g:each>
-				</tbody>
-			</table>
-			<div class="pagination">
-				<g:paginate total="${partyInstanceTotal}" />
-			</div>
+			
+			<!-- jqgrid -->
+			<jqgrid:wrapper id="party" />
 		</div>
 	</body>
 </html>
