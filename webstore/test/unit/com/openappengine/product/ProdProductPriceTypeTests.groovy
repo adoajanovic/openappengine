e88e876fdaadbb4c433bcb7766d02a8f4325e0cf
nachi1313 <nachi1313@gmail.com>
2012-07-03 13:10:42 +0000

    Test for New Domain Classes created based on Hibernate Classes no Logical change

---
+++ webstore/test/unit/com/openappengine/product/ProdProductPriceTypeTests.groovy
@@ -0,0 +1,17 @@
+package com.openappengine.product
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(ProdProductPriceType)
+class ProdProductPriceTypeTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
