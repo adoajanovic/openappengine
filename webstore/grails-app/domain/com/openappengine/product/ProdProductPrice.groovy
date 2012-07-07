e100e570abe3100a4e60d6157af68fc53cce370b
nachi1313 <nachi1313@gmail.com>
2012-07-07 23:06:02 +0000

    Domain classes created for Product, GemStone and ProductPrice

---
+++ webstore/grails-app/domain/com/openappengine/product/ProdProductPrice.groovy
@@ -0,0 +1,17 @@
+package com.openappengine.product
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+class ProdProductPrice {
+	
+	String ppCurrencyUomId
+	Date ppFromDate
+	BigDecimal ppPrice
+	Date ppToDate
+	
+	static belogsTo = [prodProduct : Product]
+
+    static constraints = {
+    }
+}
