511519e95ac000ae7642d14a6fff8ffce24b8972
nachi1313@gmail.com <nachi1313@gmail.com@7f5eb347-7fd8-1a0d-9338-d8be80e1c18b>
2012-07-07 23:06:02 +0000

    Domain classes created for Product, GemStone and ProductPrice

---
+++ branches/ecommerce/webstore/grails-app/domain/com/openappengine/product/ProductGemStone.groovy
@@ -0,0 +1,21 @@
+package com.openappengine.product
+
+import java.math.BigDecimal;
+
+class ProductGemStone extends Product{
+	
+	BigDecimal carat
+	String measurements
+	String shape
+	String cut
+	String color
+	String clarity
+	String grade
+	String origin
+	String hardness
+	String traetement
+	
+
+    static constraints = {
+    }
+}
