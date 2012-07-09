5b8b39884706f3dedbd228563f2c15654851a14c
hrishi2323 <hrishi2323@gmail.com>
2012-07-09 13:55:06 +0000

    Domain Class CSS Changes Product Filter Page Layout.

---
+++ webstore/test/unit/com/openappengine/common/ImageInfoTests.groovy
@@ -0,0 +1,17 @@
+package com.openappengine.common
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(ImageResource)
+class ImageInfoTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
